67510642c120be8f006990701ef7bc06ea64cfc1
Giovimax <giovimax@gmail.com>
2018-11-23 22:15:42 +0100

    improved interruption andling

--- automaticPreparation.py
+++ automaticPreparation.py
@@ -12,13 +12,32 @@ import datetime
 from copy import copy
 
 pathName = ""
+
+toSkip = []
+
+if "log.txt" in os.listdir(pathName):
+    with open("log.txt","r+") as log:
+        line = log.readline()
+        while line:
+            print("Read %s, set to be skipped" % line)
+            toSkip.append(line.split())
+            
+
+
+log = open("log.txt","a+")
 start = copy(datetime.datetime.now())
 print("Starting the process...","time:",start)
 
 for file in os.listdir(pathName):
-    print("preparing:", file)
-    print("Time:", datetime.datetime.now())
-    dpf.prepare()
-    print("done")
-
-print("Process finished at:", datetime.datetime.now())
+    if file in toSkip:
+        print("Skipping %s" %file)
+        pass
+    else:
+        print("preparing:", file)
+        print("Time:", datetime.datetime.now())
+        dpf.prepare()
+        print("done")
+finish = datetime.datetime.now()
+print("Process finished at:", finish)
+print("timedelta= {}".format(datetime.timedelta(start,finish)))
+log.close()
